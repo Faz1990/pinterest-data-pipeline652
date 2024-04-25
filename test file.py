34d9504e7d63327d9e7b7e24801a7c45f7bef936
Faisal Ahmed <142679287+Faz1990@users.noreply.github.com>
2024-04-25 16:12:59 +0100

    Update test file.py

--- test file.py
+++ test file.py
@@ -1,20 +1,135 @@
 import requests
+from time import sleep
+import random
+from multiprocessing import Process
+import boto3
 import json
+import sqlalchemy
+from sqlalchemy import text
 
-example_df = {"index": 1, "name": "Maya", "age": 25, "role": "engineer"}
 
-invoke_url = "https://fqso1f4f05.execute-api.us-east-1.amazonaws.com/beta"
-#To send JSON messages you need to follow this structure
-payload = json.dumps({
-    "records": [
-        {
-        #Data should be send as pairs of column_name:value, with different columns separated by commas       
-        "value": {"index": example_df["index"], "name": example_df["name"], "age": example_df["age"], "role": example_df["role"]}
-        }
-    ]
-})
+random.seed(100)
 
-headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
-response = requests.request("POST", invoke_url, headers=headers, data=payload)
 
-print(response.status_code)
+class AWSDBConnector:
+
+    def __init__(self):
+
+        self.HOST = "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
+        self.USER = 'project_user'
+        self.PASSWORD = ':t%;yCY3Yjg'
+        self.DATABASE = 'pinterest_data'
+        self.PORT = 3306
+        
+
+        
+    def create_db_connector(self):
+        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
+        return engine
+
+
+new_connector = AWSDBConnector()
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    raise TypeError("Type not serializable")
+
+def send_data_to_api(topic_suffix, data):
+    base_url = "https://fqso1f4f05.execute-api.us-east-1.amazonaws.com/beta"
+    topic_url = f"{base_url}/topics/{topic_suffix}"
+    headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'} 
+    try:
+        response = requests.post(topic_url, data=json.dumps(data, default=json_serial), headers=headers)
+        if response.status_code == 200:
+            print(f"Successfully sent data to {topic_suffix} topic.")
+        else:
+            print(f"Failed to send data to {topic_suffix} topic. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def run_infinite_post_data_loop():
+    while True:
+
+        sleep(random.randrange(0, 2))
+        random_row = random.randint(0, 11000)
+        engine = new_connector.create_db_connector()
+
+        with engine.connect() as connection:
+
+            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+            pin_selected_row = connection.execute(pin_string)
+            
+            for row in pin_selected_row:
+                pin_result = dict(row._mapping)
+                topic_name = "0ec6d756577b.pin"
+                payload = json.dumps({
+                "records": [
+                    {   
+                    "value": {"index": pin_result["index"], 
+                            "unique_id": pin_result["unique_id"], 
+                            "title": pin_result["title"], 
+                            "description": pin_result["description"], 
+                            "poster_name": pin_result["poster_name"], 
+                            "follower_count": pin_result["follower_count"], 
+                            "tag_list": pin_result["tag_list"], 
+                            "is_image_or_video": pin_result["is_image_or_video"], 
+                            "image_src": pin_result["image_src"], 
+                            "downloaded": pin_result["downloaded"], 
+                            "save_location": pin_result["save_location"], 
+                            "category": pin_result["category"]}
+                        }
+                    ]
+                })
+                send_data_to_api(pin_result, topic_name, payload)
+                print ("pin result sent")
+
+
+            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+            geo_selected_row = connection.execute(geo_string)
+            
+            for row in geo_selected_row:
+                geo_result = dict(row._mapping)
+                topic_name = "0ec6d756577b.geo"
+                payload = json.dumps({
+                "records": [
+                    {   
+                    "value": {"ind": geo_result["ind"], 
+                            "timestamp": geo_result["timestamp"].isoformat(), 
+                            "latitude": geo_result["latitude"], 
+                            "longitude": geo_result["longitude"], 
+                            "country": geo_result["country"]}
+                        }
+                    ]
+                })
+                send_data_to_api(geo_result, topic_name, payload)
+                print ("geo result sent")
+
+            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+            user_selected_row = connection.execute(user_string)
+            
+            for row in user_selected_row:
+                user_result = dict(row._mapping)
+                topic_name = "0ec6d756577b.user"
+                payload = json.dumps({
+                "records": [
+                    {   
+                    "value": {"ind": user_result["ind"], 
+                            "first_name": user_result["first_name"], 
+                            "last_name": user_result["last_name"], 
+                            "age": user_result["age"], 
+                            "date_joined": user_result["date_joined"].isoformat()}
+                        }
+                    ]
+                })
+                send_data_to_api(user_result, topic_name, payload)
+                print ("user result sent")
+
+            # print(pin_result)
+            # print(geo_result)            
+            # print(user_result)
+
+if __name__ == "__main__":
+    run_infinite_post_data_loop()
+    print('Working')
